9e8494051e20a8e1e97532d31acf66b22146847a
DuyOnix <dtduy2210@gmail.com>
2021-01-03 23:25:29 +0700

    final UBER

--- js/main.js
+++ js/main.js
@@ -9,52 +9,41 @@ var uberBlack = [10000, 16000, 14000];
 // Thứ tự lần lượt giá tiền đường đi trong bảng UBER Black
 var distanceTable = [1, 20];
 // Khoảng cách tối đa cho một gói cước được áp dụng
-var pricePerTime = [2000, 3000, 4000];
+var pricePerTimeDelay = [2000, 3000, 4000];
 // Số tiền trung bình mỗi phút chờ lần lượt của UBER X, UBER SUV, UBER Black
 
 // END GLOBAL VARIABLES 
 
-function calculateMoney(car, distance, time) {
+function calculateMoney(distance, time, vehicle) {
+    // 0 < Số KM <= 1
+    if (distance > 0 && distance <= distanceTable[0]) {
+        money = vehicle[0] + time * pricePerTimeDelay[0];
+    }
+    // 1 < Số KM <= 20
+    else if (distance > distanceTable[0] && distance <= distanceTable[1]) {
+        money = vehicle[0] + (distance - distanceTable[0]) * vehicle[1] + time * pricePerTimeDelay[0];
+    }
+    // Số KM > 20
+    else {
+        money = vehicle[0] + (distanceTable[1] - distanceTable[0]) * vehicle[1] + (distance - distanceTable[1]) * vehicle[2] + time * pricePerTimeDelay[0];
+    }
+    return money;
+}
+
+function getMoney(car, distance, time) {
     var money = 0;
     switch (car) {
         // Truong hop chon UBER X
         case "uberX":
-            // 0 < Số KM <= 1
-            if (distance > 0 && distance <= distanceTable[0]) {
-                money = uberX[0] + time * pricePerTime[0];
-            }
-            // 1 < Số KM <= 20
-            else if (distance > distanceTable[0] && distance <= distanceTable[1]) {
-                money = uberX[0] + (distance - distanceTable[0]) * uberX[1] + time * pricePerTime[0];
-            }
-            // Số KM > 20
-            else {
-                money = uberX[0] + (distanceTable[1] - distanceTable[0]) * uberX[1] + (distance - distanceTable[1]) * uberX[2] + time * pricePerTime[0];
-            }
+            money = calculateMoney(distance, time, uberX);
             break;
         // Truong hop chon UBER SUV   
         case "uberSUV":
-            if (distance > 0 && distance <= distanceTable[0]) {
-                money = uberSUV[0] + time * pricePerTime[1];
-            }
-            else if (distance > distanceTable[0] && distance <= distanceTable[1]) {
-                money = uberSUV[0] + (distance - distanceTable[0]) * uberSUV[1] + time * pricePerTime[1];
-            }
-            else {
-                money = uberSUV[0] + (distanceTable[1] - distanceTable[0]) * uberSUV[1] + (distance - distanceTable[1]) * uberSUV[2] + time * pricePerTime[1];
-            }
+            money = calculateMoney(distance, time, uberSUV);
             break;
         // Truong hop chon UBER Black
         case "uberBlack":
-            if (distance > 0 && distance <= distanceTable[0]) {
-                money = uberBlack[0] + time * pricePerTime[2];
-            }
-            else if (distance > distanceTable[0] && distance <= distanceTable[1]) {
-                money = uberBlack[0] + (distance - distanceTable[0]) * uberBlack[1] + +time * pricePerTime[2];
-            }
-            else {
-                money = uberBlack[0] + (distanceTable[1] - distanceTable[0]) * uberBlack[1] + (distance - distanceTable[1]) * uberBlack[2] + time * pricePerTime[2];
-            }
+            money = calculateMoney(distance, time, uberBlack);
             break;
         default:
             break;
@@ -81,6 +70,9 @@ document.getElementById("tinhTien").addEventListener("click", function () {
     var car = getCar();
     var distance = document.getElementById("distance").value;
     var time = document.getElementById("time").value;
+    // Set warning input
+    document.getElementById("warn-km").style.display = "none";
+    document.getElementById("warn-time").style.display = "none";
     // Test input
     if (car == "") {
         alert("Vui lòng chọn loại xe");
@@ -90,21 +82,26 @@ document.getElementById("tinhTien").addEventListener("click", function () {
         alert("Vui lòng nhập vào số KM");
         return;
     }
-    if (distance <= 0) {
-        alert("Vui lòng nhập đúng số KM (km > 0)");
-        return;
-    }
     if (time == "") {
         alert("Vui lòng nhập vào thời gian chờ");
         return;
     }
-    if (time <= 0) {
-        alert("Vui lòng nhập đúng thời gian chờ (time > 0)");
+    if (distance <= 0 && time <= 0) {
+        document.getElementById("warn-km").style.display = "block";
+        document.getElementById("warn-time").style.display = "block";
+        return;
+    }
+    else if (distance <= 0) {
+        document.getElementById("warn-km").style.display = "block";
+        return;
+    }
+    else if (time <= 0) {
+        document.getElementById("warn-time").style.display = "block";
         return;
     }
     // END Test input
 
-    var money = calculateMoney(car, distance, time);
+    var money = getMoney(car, distance, time);
     document.getElementById("divThanhTien").style.display = "block";
     document.getElementById("xuatTien").innerHTML = money;
 });
@@ -139,6 +136,7 @@ function getBill(distance, time, money, vehicle) {
     // Số KM > 20
     else {
         document.getElementById("line_1").style.display = "table-row";
+        var getFirstLine = document.querySelectorAll("#bill table #line_1 td");
         getFirstLine[0].innerHTML = ubers[0];
         getFirstLine[1].innerHTML = distanceTable[0] + " km";
         getFirstLine[2].innerHTML = vehicle[0];
@@ -159,11 +157,11 @@ function getBill(distance, time, money, vehicle) {
         getFirstLine[3].innerHTML = (distance - distanceTable[1]) * vehicle[2];
     }
 
-    // In thời gian chờ
+    // In số tiền khoảng thời gian chờ
     var getTimeBill = document.querySelectorAll("#bill table #line_4 td");
     getTimeBill[1].innerHTML = time + " phút";
-    getTimeBill[2].innerHTML = pricePerTime[0];
-    getTimeBill[3].innerHTML = time * pricePerTime[0];
+    getTimeBill[2].innerHTML = pricePerTimeDelay[0];
+    getTimeBill[3].innerHTML = time * pricePerTimeDelay[0];
 
     // In tổng tiền
     document.getElementById("totalBill").innerHTML = money;
@@ -173,7 +171,7 @@ document.getElementById("inHoaDon").addEventListener("click", function () {
     var car = getCar();
     var distance = document.getElementById("distance").value;
     var time = document.getElementById("time").value;
-    var money = calculateMoney(car, distance, time);
+    var money = getMoney(car, distance, time);
     switch (car) {
         case "uberX":
             getBill(distance, time, money, uberX);
